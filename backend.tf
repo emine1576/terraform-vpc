775a6dc64f96f119fba6a4d78affbab2f8d213b7
emine1576 <84200449+emine1576@users.noreply.github.com>
2021-09-16 22:04:24 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "myjenkins-yavuz"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
