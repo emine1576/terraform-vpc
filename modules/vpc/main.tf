8c4c6d186a7b1d249534a88a2813690845719f3d
emine1576 <84200449+emine1576@users.noreply.github.com>
2021-09-17 14:12:28 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,16 +62,6 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# NAT EIPs
-resource "aws_eip" "nat_eip" {
-  vpc   = true
-  count = length(aws_subnet.public_subnets)
-
-  tags = {
-    Name = "${var.environment}_nat_eip"
-  }
-}
-
 # NAT Gateway
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = element(aws_eip.nat_eip.*.id, count.index)
